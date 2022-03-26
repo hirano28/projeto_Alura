e05447bf42d13f0b6f35e84519b3a75d40a7d7f6
Edson Hirano <edson.hriano28@gmail.com>
2022-03-26 11:43:50 -0300

    Estrutura inicial API

--- src/app.js
+++ src/app.js
@@ -1,34 +1,48 @@
 import express from "express";
 
 const app = express();
+
 app.use(express.json())
 
 const livros = [
-    {id: 1, titulo: "Harry Potter e o Cálice de Fogo"},
-    {id: 2, titulo: "O Hobbit"}
+  {id: 1, "titulo": "Senhor dos Aneis"},
+  {id: 2, "titulo": "O Hobiit"}
 ]
 
 app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node');
-})
+  res.status(200).send('Curso de Node');
+}) 
 
 app.get('/livros', (req, res) => {
-    res.status(200).json(livros);
+  res.status(200).json(livros)
+})
+
+app.get('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  res.json(livros[index]);
 })
 
 app.post('/livros', (req, res) => {
-    const livro = req.body;
-    livros.push(livro);
-    res.status(201).send('Livro cadastrado com sucesso.');
+  livros.push(req.body);
+  res.status(201).send('Livro foi cadastrado com sucesso')
 })
 
 app.put('/livros/:id', (req, res) => {
-    const livro = buscaLivro(req.params.id);
-
+  let index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
 })
 
+app.delete('/livros/:id', (req, res) => {
+  let {id} = req.params;
+  let index = buscaLivro(id);
+  livros.splice(index, 1);
+  res.send(`Livro ${id} removido com sucesso`);
+})
+
+
 function buscaLivro(id) {
-    return this.livros.find(livro => livro.id == id);
+  return livros.findIndex(livro => livro.id == id)
 }
 
 export default app
--- src/model/livro.ts
+++
@@ -1,5 +0,0 @@
-
-export interface Livro {
-    id: number,
-    titulo: string
-}
